08b54c86d2cabda12915d8b2a9cfb3d557a6f32c
shiwan66 <shiwan66@gmail.com>
2018-12-30 11:09:32 +0800

    init

---
+++ kubectl-apply.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+kubectl apply -f registry/
+kubectl apply -f detection/
